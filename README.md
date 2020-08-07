b94bc5195415e9bac2930dfbd5d4bb19ca2ff4fd
Daniel Vijge <danielvijge@users.noreply.github.com>
2020-08-07 09:42:07 +0200

    Use Github Actions to build release

--- README.md
+++ README.md
@@ -8,12 +8,15 @@ Press the _Apply_ button and restart LMS.
 After installation, you can configure the Plugin under _Settings_ > _Advanced_ > _Mixcloud_
 
 The plugin is included as a default third party resource. It is distributed via my
-[personal repository](http://server.vijge.net/squeezebox/) There is no need
-to add an additional repository, because this is an official third party repository.
-However, if you want to, you can add the repository directly. A new version might be
-earlier available in this repository:
+[personal repository](http://server.vijge.net/squeezebox/) This third party repository
+is synced with the repository XML files on GitHub. It is also possible to directly include
+the repository XML from GitHub. For the release version, include
     
     http://danielvijge.github.io/lms_mixcloud/public.xml
+
+For the development version (updated with every commit), include
+
+    http://danielvijge.github.io/lms_mixcloud/public-dev.xml
 
 This Plugin is in Alpha stage and build from the SqueezeCloud Plugin (thanks to the developers), because the documentation
 of the LMS Server is very bad and Perl still sucks.
--- generate-zip.sh
+++ generate-zip.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
 set -x
 
-VERSION=$(grep \<version\> install.xml  | perl -n -e '/>(.*)</; print $1;')
+VERSION=`git describe --tags`
 
-cd ..
-mv lms_mixcloud MixCloud
-rm lms_mixcloud-$VERSION.zip
-zip -r lms_mixcloud-$VERSION.zip MixCloud -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\*
-mv MixCloud lms_mixcloud
-SHA=$(shasum lms_mixcloud-$VERSION.zip | awk '{print $1;}')
-
-cat <<EOF > public.xml
-<extensions>
-	<details>
-		<title lang="EN">Mixcloud Plugin</title>
-	</details>
-	<plugins>
-		<plugin name="MixCloud" version="$VERSION" minTarget="7.5" maxTarget="*">
-			<title lang="EN">Mixcloud</title>
-			<desc lang="EN">Play music from Mixcloud</desc>
-			<url>http://danielvijge.github.io/lms_mixcloud/lms_mixcloud-$VERSION.zip</url>
-			<link>https://github.com/danielvijge/lms_mixcloud</link>
-			<sha>$SHA</sha>
-			<creator>Christian Mueller, Daniel Vijge</creator>
-		</plugin>
-	</plugins>
-</extensions>
-EOF
+sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml  > install.xml
+zip -r lms_mixcloud-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml
+rm install.xml
