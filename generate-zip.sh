1fc1def836d65b480a146f1cfaf2a878d7861e85
Daniel Vijge <danielvijge@users.noreply.github.com>
2020-08-10 20:33:18 +0200

    Make versions number work for auto upgrade in LMS

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-VERSION=`git describe --tags`
+VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`
 
 sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml  > install.xml
 zip -r lms_mixcloud-$VERSION.zip . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml
