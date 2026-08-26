94062d96a05ee8e20b597c49da8b88e5b7a2890c
Daniel Vijge <danielvijge@users.noreply.github.com>
2026-08-26 21:15:21 +0200

    Improve script to generate local builds

--- generate-zip.sh
+++ generate-zip.sh
@@ -1,23 +1,74 @@
 #!/bin/sh
-set -x
 
-VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`
+while [[ $# -gt 0 ]]; do
+  case ${1} in
+    -v|--version)
+      VERSION="${2}"
+      shift # past argument
+      shift # past value
+      ;;
+    -a|--all-platforms)
+      ALL_PLATFORMS="1"
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option ${1}"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("${1}") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
 
-sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml  > install.xml
+function get_ytdlp_name() {
+	local PLATFORM=${1}
+	local FILENAME=yt-dlp
+	case "${PLATFORM}" in
+		"windows")   FILENAME=${FILENAME}.exe;;
+		"macos")     FILENAME=${FILENAME}_macos;;
+		*);;
+	esac
+	echo ${FILENAME}
+}
 
+function download_yt_dlp() {
+	local PLATFORM=${1}
+	local FILENAME="$(get_ytdlp_name ${PLATFORM})"
+	if [ ! -f Bin/${YT_VERSION}/${FILENAME} ]; then
+		wget https://github.com/yt-dlp/yt-dlp/releases/download/${YT_VERSION}/${FILENAME} -P Bin/${YT_VERSION}
+		if [ "${PLATFORM}" != "windows" ]; then
+			chmod +x Bin/${YT_VERSION}/${FILENAME}
+		fi
+	fi
+	rm -f Bin/yt-dlp*
+	if [ "${PLATFORM}" == "windows" ]; then EXTENSION=.exe; fi
+	cp Bin/${YT_VERSION}/${FILENAME} Bin/yt-dlp${EXTENSION}
+
+}
+
+function generate_zip() {
+	local PLATFORM=${1}
+	local ZIPFILE=lms_mixcloud-$VERSION-${PLATFORM}.zip
+	echo "Creating ${ZIPFILE}..."
+	download_yt_dlp ${PLATFORM}
+	rm -f ${ZIPFILE}
+	zip -r ${ZIPFILE} . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml yt-dlp\.version Bin/\.gitkeep Bin/*/\*
+}
+
+if [ -z ${VERSION} ]; then
+	VERSION=`git describe --tags --abbrev=0`.`git rev-list $(git describe --tags --abbrev=0)..HEAD --count`-local
+fi
 YT_VERSION=`cat yt-dlp.version`
 
-rm Bin/yt*
-wget https://github.com/yt-dlp/yt-dlp/releases/download/${YT_VERSION}/yt-dlp -P Bin && chmod +x Bin/yt-dlp
-zip -r lms_mixcloud-$VERSION-linux.zip . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml Bin\.gitkee
+sed "s/{{ env\['VERSION'\] }}/$VERSION/g" install.template.xml > install.xml
 
-rm Bin/yt*
-wget https://github.com/yt-dlp/yt-dlp/releases/download/${YT_VERSION}/yt-dlp.exe -P Bin
-zip -r lms_mixcloud-$VERSION-windows.zip . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml Bin\.gitkeep Bin\yt-dlp
+generate_zip "linux"
+if [ ! -z ${ALL_PLATFORMS} ]; then
+	generate_zip "macos"
+	generate_zip "windows"
+fi
 
-rm Bin/yt*
-wget https://github.com/yt-dlp/yt-dlp/releases/download/${YT_VERSION}/yt-dlp_macos -P Bin && chmod +x Bin/yt-dlp_macos
-mv Bin/yt-dlp_macos Bin/yt-dlp
-zip -r lms_mixcloud-$VERSION-macos.zip . -x \*.zip \*.sh \*.git\* \*README\* \*sublime-\* \*.DS_Store\* \*.template.xml Bin\.gitkeep Bin\yt-dlp.exe
-
+rm Bin/yt-dlp*
 rm install.xml
